f5f445088772702a1701ccf35a29754dc1347339
kodak <gotomarryyou@gmail.com>
2016-06-15 08:22:14 +0800

    Rejected commit: Default

--- app/src/main/java/com/kd/seeweico/common/setting/Setting.java
+++ app/src/main/java/com/kd/seeweico/common/setting/Setting.java
@@ -1,5 +1,7 @@
 package com.kd.seeweico.common.setting;
 
+import com.alibaba.fastjson.JSON;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,5 +12,18 @@ import java.util.Map;
 public class Setting extends SettingBean implements Serializable {
     private static final long serialVersionUID = 1399762922574268795L;
     private Map<String, Setting> extras = new HashMap<>();
-    
+
+    public Setting() {}
+
+    public Map<String, Setting> getExtras() {
+        return this.extras;
+    }
+
+    public void setExtras(Map<String, Setting> extras) {
+        this.extras = extras;
+    }
+
+    public Setting copy() {
+        return JSON.parseObject(JSON.toJSONString(this), Setting.class);
+    }
 }
